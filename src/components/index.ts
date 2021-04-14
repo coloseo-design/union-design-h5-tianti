e6d5e6471998da39e586ed4b136bcf58e81e345c
peng.deng <peng.deng@coloseo.cn>
2021-04-14 14:14:16 +0800

    feat: 🎸 完成项目框架搭建

---
+++ src/components/index.ts
@@ -0,0 +1,18 @@
+const ENV = process.env.NODE_ENV;
+if (
+  ENV !== 'production'
+  && ENV !== 'test'
+  && typeof console !== 'undefined'
+  && console.warn // eslint-disable-line no-console
+  && typeof window !== 'undefined'
+) {
+  // eslint-disable-next-line no-console
+  console.warn(
+    `You are using a whole package of antd,
+      please use https://www.npmjs.com/package/babel-plugin-import to reduce app bundle size.`,
+  );
+}
+
+// 请在此处导出所有组件
+export { default as Icon } from './icon';
+export { default as Button } from './button';
