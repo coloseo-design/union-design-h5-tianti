d62721dc74bf03a613f03004603c574296fd9539
caoqiyue <ca1308449030@163.com>
2021-04-20 13:13:33 +0800

    collapse 折叠面板完成

---
+++ src/components/collapse/collapse.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import classNames from 'classnames';
+import { ConfigConsumer, ConfigConsumerProps } from '../config-provider/context';
+import CollapsePanel from './collapsePanel';
+
+export interface CollapseProps {
+    activeKey?:number;
+    defaultActiveKey?:number;
+    onChange?:(value:number| undefined, show:boolean) => void;
+    className?: string;
+    prefixCls?: string;
+    accordion?: boolean;
+    style?: {[key: string]: unknown};
+}
+
+class Collapse extends React.Component<CollapseProps> {
+    static Panel = CollapsePanel;
+
+    renderCollapse = ({ getPrefixCls }: ConfigConsumerProps) => {
+      const {
+        prefixCls,
+        className,
+        defaultActiveKey,
+        activeKey,
+        style,
+        children,
+        accordion,
+      } = this.props;
+      const prefix = getPrefixCls('collapse-mobile', prefixCls);
+      const clazzName = classNames(prefix, {
+        [`${prefix}-accordion`]: accordion,
+      }, className);
+      return (
+        <div className={clazzName} style={{ ...style }}>
+          {
+          React.Children.map(children, (item:React.ReactNode, index:number) => {
+            if (item && typeof item === 'object' && 'props' in item) {
+              const key = item.key ? parseInt(`${item.key}`, 10) : index;
+              const props = {
+                show: key === defaultActiveKey || key === activeKey,
+                accordion,
+                ...item.props,
+              };
+              return <CollapsePanel {...props} />;
+            }
+            return null;
+          })
+          }
+        </div>
+      );
+    };
+
+    render() {
+      return (
+        <ConfigConsumer>
+          {this.renderCollapse}
+        </ConfigConsumer>
+      );
+    }
+}
+
+Collapse.Panel = CollapsePanel;
+
+export default Collapse;
