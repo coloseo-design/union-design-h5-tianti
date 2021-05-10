b22b3de9b386c893a8188b307a7cfe313ef33b72
jc-liuy23 <jc-liuy23@chinaunicom.cn>
2021-05-10 17:08:14 +0800

    feat: 🎸 apidoc

--- src/components/tab/apidoc.js
+++ src/components/tab/apidoc.js
@@ -1,4 +1,4 @@
 module.exports = {
 	name: "Tab 标签页组件",
-	content: {"tab-Tab":["Tab"],"tab-TabItem":["Tab.Item"],"tab-TabConfig全局配置":["Tab.Config 全局配置"],"tab-代码演示":["代码演示"]}
+	content: {"tab-Tab标签页组件":["Tab 标签页组件"],"tab-Tab":["Tab"],"tab-TabItem":["Tab.Item"],"tab-TabConfig全局配置":["Tab.Config 全局配置"],"tab-代码演示":["代码演示"]}
 };
