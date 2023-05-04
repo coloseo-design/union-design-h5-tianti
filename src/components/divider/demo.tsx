91ada95e11ac503af26e6c3f30fae433401117f2
ES1993 <18981810422@163.com>
2023-05-04 16:24:32 +0800

    Divider ok

--- src/components/divider/demo.tsx
+++ src/components/divider/demo.tsx
@@ -2,15 +2,6 @@ import React from "react";
 import { Divider } from "../index";
 import "./styles/index";
 
-const containerStyle = {
-  width: 377,
-  height: 548,
-  backgroundColor: "#fafafa",
-  padding: 10,
-  overflow: "scroll",
-  borderRadius: 12,
-  boxShadow: "#ebedf0 0 4px 12px",
-};
 const DividerDemo = () => (
   <div>
     <p>
