40b2ffa9ee777429ac0b9d01a97a3c8a97b40449
liao.yj <liao.yingju@cyberway.net.cn>
2023-05-29 17:18:17 +0800

    approve 增加status状态

--- src/components/approve-card/demo.tsx
+++ src/components/approve-card/demo.tsx
@@ -60,9 +60,29 @@ export default () => {
   }, []);
   return (
     <div>
+      <ApproveCard
+        title="莫某某提交的财务申请相关内容莫某某提交的财务申请相关内容相关内容"
+        status="progress"
+        description="地址信息一级-地址相关信息二级"
+      />
+      <ApproveCard
+        title="莫某某提交的财务申请相关内容莫某某提交的财务申请相关内容相关内容"
+        status="success"
+        description="地址信息一级-地址相关信息二级"
+      />
+      <ApproveCard
+        title="莫某某提交的财务申请相关内容莫某某提交的财务申请相关内容相关内容"
+        status="rejected"
+        description="地址信息一级-地址相关信息二级"
+      />
+            <ApproveCard
+        title="莫某某提交的财务申请相关内容莫某某提交的财务申请相关内容相关内容"
+        status={<div style={{ padding: '0px 2px', textAlign: 'center', lineHeight: '16px', fontSize: 12, borderRadius: 4, color: 'palegreen', background: 'green'}}>自定义</div>}
+        description="地址信息一级-地址相关信息二级"
+      />
       {visible && <ApproveCard
         title="莫某某提交的财务申请相关内容莫某某提交的财务申请相关内容相关内容"
-        status="进行中"
+        status="progress"
         description="地址信息一级-地址相关信息二级"
       />}
       <div style={{ background: '#fff' }}>
--- src/components/approve-card/index.tsx
+++ src/components/approve-card/index.tsx
@@ -1,22 +1,37 @@
-import React, { HtmlHTMLAttributes, ReactNode, useContext } from 'react';
+import React, {
+  HtmlHTMLAttributes, ReactNode, useContext, isValidElement,
+} from 'react';
+import classNames from 'classnames';
 import { ConfigContext } from '../config-provider/context';
 
 interface ApproveCardProps extends Omit<HtmlHTMLAttributes<HTMLDivElement>, 'title'> {
   title: string | ReactNode;
-  status?: string | ReactNode;
+  status?: 'progress' | 'rejected' | 'success' | ReactNode;
   description?: string | ReactNode;
 }
 const ApproveCard: React.FC<ApproveCardProps> = (props) => {
   const {
     title, status, description, ...rest
   } = props;
+  const statusMap: {[x: string]: string} = {
+    progress: '进行中',
+    rejected: '已拒绝',
+    success: '已通过',
+  };
   const { getPrefixCls } = useContext(ConfigContext);
   const prefix = getPrefixCls('approve-card');
   return (
     <div className={prefix} {...rest}>
       <div title={title as string} className={`${prefix}-title`}>{title}</div>
       <div className={`${prefix}-sub`}>
-        <div className={`${prefix}-sub-status`}>{status}</div>
+        {isValidElement(status) ? status : (
+          <div className={classNames(`${prefix}-sub-status`, {
+            [`${prefix}-sub-${status}`]: status,
+          })}
+          >
+            {statusMap[status as string]}
+          </div>
+        )}
         <div className={`${prefix}-sub-description`}>{description}</div>
       </div>
     </div>
