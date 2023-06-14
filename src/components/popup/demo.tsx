b7158c2f43481c5707c805329361183b51c10c0d
liao.yj <liao.yingju@cyberway.net.cn>
2023-06-14 18:11:27 +0800

    popup 方向fix

--- src/components/popup/demo.tsx
+++ src/components/popup/demo.tsx
@@ -69,7 +69,7 @@ const PopupDemo = () => {
       <Popup
         closeable={false}
         visible={fullVisible}
-        position='left'
+        position='bottom'
         onCancel={handleCancel}
         fullScreen
         footer={null}
